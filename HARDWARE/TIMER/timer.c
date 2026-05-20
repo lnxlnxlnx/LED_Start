2cb52b3f4f85ae80ea247958e889a5b3d278a99a
lnx <a16725798566@163.com>
2026-05-20 18:52:40 +0800

    9.4.1 可以运行的红外遥控

--- HARDWARE/TIMER/timer.c
+++ HARDWARE/TIMER/timer.c
@@ -203,7 +203,7 @@ extern void adc_irq_func(void);
 #define USE_LED      0
 #define USE_REMOTE   1
 #define USE_IC_UPDATE 0
-#define USE_ADC_REF  1
+#define USE_ADC_REF  0
 
 void TIM3_IRQHandler(void)
 {
@@ -231,7 +231,7 @@ extern u16 TIM3_ONE_SECOND_COUNT;
 
 // ── TIM4 ─────────────────────────────────────────────
 extern void remote_irq_func(void);
-#define USE_REMOTE_SMG_IRQ 0
+#define USE_REMOTE_SMG_IRQ 1
 void TIM4_IRQHandler(void)
 {
     if (TIM4->SR & 0X0001)
